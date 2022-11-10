97a957c73c540a86b817df548a9e6108b7d71969
Sourav Saha <shanto.sourav07@gmail.com>
2022-11-10 15:19:26 +0600

    JWT Done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Server Side
+# Assignment 10: Pixology Server Side
+## Link: [Pixology](https://programerly-server-side.vercel.app)
+
+## ProgramerLy: 
+Pixology is a personal photographer website. You can see my different services and add a review of these services after login. All the services data are from mongobd.
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+var jwt = require('jsonwebtoken');
 
 // middle wares
 app.use(cors());
@@ -12,10 +13,40 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0nty043.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         const serviceCollection = client.db('pixology').collection('services');
         const reviewCollection = client.db('pixology').collection('reviews');
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+            res.send({ token })
+        })
+
+        app.get('/reverse-services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
 
         app.get('/services', async (req, res) => {
             const query = {}
@@ -55,7 +86,13 @@ async function run() {
         });
 
         //Review
-        app.get('/review', async (req, res) => {
+        app.get('/review', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'unauthorized access' })
+            }
+
             let query = {};
 
             if (req.query.email) {
